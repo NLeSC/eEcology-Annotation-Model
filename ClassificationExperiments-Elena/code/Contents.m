d5e5d47f5c15dc8088a366a16c30ea57ea780df1
elboyran <e.ranguelova@esciencecenter.nl>
2013-08-15 17:38:10 +0200

    first scripts

---
+++ ClassificationExperiments-Elena/code/Contents.m
@@ -0,0 +1,8 @@
+% Contents.m- the contents of ClassificationExperiments_Elena/code directory
+%
+% last update date: 15-08-2013
+%
+%---------------------------------------------------------------------------
+% FUNCTIONS
+%---------------------------------------------------------------------------
+% function [data]=GetDataFromDB(username, password)- Function to get the accellerometer data from the eecology DB
---
+++ ClassificationExperiments-Elena/code/GetDataFromDB.m
@@ -0,0 +1,10 @@
+% function [data]=GetDataFromDB(username, password)
+%
+% Function to get the accellerometer data from the eecology DB
+% 
+% INPUTS:
+%
+% OUPTPUTS:
+%
+% author: Elena Ranguelova
+% date: 15-08-2013
