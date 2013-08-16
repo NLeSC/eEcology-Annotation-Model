3572248da0b234f03137e207f38842e86f6f5d9a
elboyran <e.ranguelova@esciencecenter.nl>
2013-08-16 11:45:48 +0200

    file rename

--- ClassificationExperiments-Elena/code/Contents.m
+++ ClassificationExperiments-Elena/code/Contents.m
@@ -1,8 +1,13 @@
 % Contents.m- the contents of ClassificationExperiments_Elena/code directory
 %
-% last update date: 15-08-2013
-%
+% last update date: 16-08-2013
+%---------------------------------------------------------------------------
+% TEMPLATES
+%---------------------------------------------------------------------------
+% template.m - generic header for a function or script
+% 
 %---------------------------------------------------------------------------
 % FUNCTIONS
 %---------------------------------------------------------------------------
-% function [data]=GetDataFromDB(username, password)- Function to get the accellerometer data from the eecology DB
+% getDataFromDB.m - Function to get the GPS, accellerometer and speed data
+%                   from the eecology DB for given device
