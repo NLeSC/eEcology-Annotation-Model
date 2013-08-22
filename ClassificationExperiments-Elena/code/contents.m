a7435b9afdf342536f6c9ff286aeed525acbc023
elboyran <e.ranguelova@esciencecenter.nl>
2013-08-22 17:29:54 +0200

    update

--- ClassificationExperiments-Elena/code/contents.m
+++ ClassificationExperiments-Elena/code/contents.m
@@ -11,13 +11,16 @@
 %--------------------------------------------------------------------------
 % getDataFromEecologyDB - getting data from the eEcology UvA DB
 % formatDataStructure - formatting the data retrieved from the EcologyDB
+% prepareData4FeatExtraction - preparation of data for feature extraction
 %
 % calcFeatureVectors - feature vectors computaion for classifiers
 % calcPosition - calculates function position features
 % calcCorrelation - calculates correlation features
 % calcMotionlessness - calculates motionlessness features
 % calcFrequency - calculates frequency spectral features
-
+%
+% applyModel - classification of feature data according to a given model
+% hierarchClass - hierarchical classification
 %--------------------------------------------------------------------------
 % SCRIPTS
 %--------------------------------------------------------------------------
