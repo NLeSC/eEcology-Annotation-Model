b935ecb0abc189324df00d3e0aaa72e81faa8194
elboyran <e.ranguelova@esciencecenter.nl>
2013-10-08 13:44:16 +0200

    added necessary data for execution of the scripts

---
+++ ClassificationExperiments-Elena/data/DBQueries/DBquery325-2010-07-09.sql
@@ -0,0 +1,9 @@
+﻿SELECT a.*,(a.x_acceleration-d.x_o)/d.x_s as x_cal, 
+               (a.y_acceleration-d.y_o)/d.y_s as y_cal,
+               (a.z_acceleration-d.z_o)/d.z_s as z_cal 
+ FROM gps.uva_acceleration101 as a                join gps.uva_device d 
+               ON a.device_info_serial = d.device_info_serial where a.device_info_serial=325 and date_time between '2010-07-9 00:13:48'
+ and '2011-07-09 11:01:05' order by date_time, index
+
+
+ 
---
+++ ClassificationExperiments-Elena/data/DBQueries/DBquery538-2011-07-12_13.sql
@@ -0,0 +1,9 @@
+﻿SELECT a.*,(a.x_acceleration-d.x_o)/d.x_s as x_cal, 
+               (a.y_acceleration-d.y_o)/d.y_s as y_cal,
+               (a.z_acceleration-d.z_o)/d.z_s as z_cal 
+ FROM gps.uva_acceleration101 as a                join gps.uva_device d 
+               ON a.device_info_serial = d.device_info_serial where a.device_info_serial=538 and date_time between '2011-07-12 10:07:24'
+ and '2011-07-13' order by date_time, index
+
+
+ 
