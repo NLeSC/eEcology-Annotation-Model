0dd5c26620a29da781c2bc8f91938ab03b672ee9
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2013-10-09 14:43:14 +0200

    Add a script to be used by script wrapper.
    Use openearth for database quering.

--- ClassificationExperiments-Elena/code/getDataFromEecologyDB.m
+++ ClassificationExperiments-Elena/code/getDataFromEecologyDB.m
@@ -13,12 +13,12 @@
 %
 % INPUT
 % username, password- the string credentials for logging to the eEcology DB
-% querystorefname- filename where common queries are stored as parametreised 
+% querystorefname- filename where common queries are stored as parametreised
 %                  strings (currenlty it's in ../data/eecologyqueries.mat)
 % queryid- a query idenifier- variable name under which the query was
 %          stored in the querystorefname
 % device - device ID for which the data are to be  retrieved
-% starttime and stoptime- define the time strip of interest. 
+% starttime and stoptime- define the time strip of interest.
 %                         Strings of format 'yyy-mm-dd hh:mm:ss'
 %
 % OUPTPUT
@@ -40,31 +40,18 @@ function [data] = getDataFromEecologyDB(username, password, ...
                                         device, starttime, stoptime)
 
 %% Paramters
-IDevice = num2str(device);    
-start = strcat('''',starttime,''''); 
+IDevice = num2str(device);
+start = strcat('''',starttime,'''');
 stop = strcat('''',stoptime,'''');
 load(querystorefname, queryid);
 
- %% Connect to the DB using jdbc
- connection = database('eecology', username, password,...
-                       'org.postgresql.Driver', ...
-           'jdbc:postgresql://db.e-ecology.sara.nl:5432/eecology?ssl=true');  
- % set db preferences    
- setdbprefs('DataReturnFormat','structure');    
-    
-%% Generate/load query  
+%% Generate/load query
 sql_query = sprintf(eval(queryid), IDevice, start, stop, IDevice, start, stop);
-  
-%% Execute the query  
-cursor = exec(connection,sql_query);
 
-% this fetch command actually transfers the data from database table to matlab
-cursor = fetch(cursor);
-
-%% Get the data from the cursor
-data = cursor.Data;
-%cursor.message;
-
-%% Close the database connection
-close(connection); 
+% Find jdbc driver
+pg_settings()
+% Connect
+connection = pg_connectdb('eecology?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory', 'host', 'db.e-ecology.sara.nl', 'user', username, 'pass', password, 'database_toolbox', 0)
+% Run query
+data = pg_fetch_struct(connection, sql_query)
 
---
+++ ClassificationExperiments-Elena/code/scripts/classifcator.m
@@ -0,0 +1,106 @@
+function classificator(dbUsername, dbPassword, dbName, databaseHost, TrackerIdentifier, startTime, stopTime, data_path)
+% classification and vizualization of a track for adding to script wrapper
+%
+% INPUT
+% dbUsername is a string for connecting to the database
+% dbPassword is a string for connecting to the database
+% dbName is a string
+% databaseHost is a string hostname of the database
+% TrackerIdentifier is a tracker indentifiers, eg '800'.
+% startTime is a string in ISO8601 time format, eg. '2013-07-01T00:00:00'
+% stopTime is a string in ISO8601 time format, eg. '2013-07-01T00:00:00'
+% data_path is string of a directory where classifiers.mat and eecologyqueries.mat reside
+%
+% Command to compile:
+% mcc -mv -R -nodisplay -I openearth/io/postgresql -I openearth -I openearth/general -I openearth/general/io_fun -I code code/scripts/classificator.m
+%
+% Usage:
+% ./run_classificator.sh /opt/matlab2009b '****' '****' eecology db.e-ecology.sara.nl 800 2010-07-01T00:00:00 2010-09-01T00:00:00 /data/classifiers
+
+results_path = '.';
+
+classifiers_fname = fullfile(data_path,'classifiers.mat')
+load(classifiers_fname)
+eco_queries =  fullfile(data_path,'eecologyqueries.mat');
+query_id = 'sql_gps_acc';
+
+num_meas = 60;
+num_features= 58;
+num_classes= 7;
+windowSize=20;
+overlap=0;
+epsilon = 0.3;
+
+%filename for kmz file
+IDdate=[num2str(TrackerIdentifier) '_' startTime(1:4) startTime(6:7) startTime(9:10)...
+    '_' startTime(12:13) startTime(15:16) startTime(18:19) ...
+    '__' stopTime(1:4) stopTime(6:7) stopTime(9:10)...
+    '_' stopTime(12:13) stopTime(15:16) stopTime(18:19)];
+
+fileName=[fullfile(results_path, 'S') IDdate '.kmz'];
+iconStr = ['http://maps.google.com/','mapfiles/kml/pal2/icon26.png'];
+iconSize = 0.4;
+classText='stand flap soar walk sit XFl  float NoCl';
+dateTimeFormat = 'yyyy-mm-ddTHH:MM:SSZ';
+
+%mat-file for storing intermediate results
+StorName=[fullfile(results_path,'Stor') IDdate '.mat'];
+
+%name datacube
+DCubeName= [fullfile(results_path,'Datacube') IDdate];
+% name annotation file
+AnotName= [fullfile(results_path,'Anot') IDdate];
+% create png directory
+dirName=[results_path IDdate 'png'];
+if ~exist(dirName,'dir')
+    mkdir(dirName);
+end
+
+%% get the data from the DB
+t=clock;
+disp('Retrieving data from the DB...');
+tic
+[tracks] = getDataFromEecologyDB(dbUsername, dbPassword,...
+                               eco_queries, query_id,...
+                               TrackerIdentifier, startTime, stopTime);
+disp('Done.');
+toc
+%% format the data
+disp('Formatting the data...');
+tic
+[formatted_tracks] = formatDataStructure(tracks);
+disp('Done.');
+toc
+
+%% classify the data
+disp('Classification...');
+tic
+[class_data, FTVstor, INFO]=classifyAccMeas(formatted_tracks, num_meas, classifiers, ...
+                                    num_features, num_classes, windowSize, overlap, epsilon);
+disp('Done.');
+toc
+
+%% create Datacube
+disp('Creating Datacube...');
+tic
+Datacube = createDatacube(class_data,1,DCubeName);
+disp('Done.');
+toc
+
+%% create Anotations
+disp('Creating Annotations...');
+tic
+FTVanot = createAnot(FTVstor,num_features,1,AnotName);
+disp('Done.');
+toc
+
+%% Make kmz
+disp('Creating KMZ file...');
+tic
+makeKMZanot(class_data, iconStr, iconSize, ...
+                          classText, dateTimeFormat, dirName, fileName);
+disp('Done.');
+toc
+
+disp('Total elapsed time: ');
+etime(clock, t)
