db2df80d98fd5c23acbce03677bd78864d852b2a
elboyran <e.ranguelova@esciencecenter.nl>
2013-11-28 17:22:35 +0100

    adding KMZ file generation for the FEBO data

--- ClassificationExperiments-Elena/code/Classification/scripts/DBAcc_FEBO_new.m
+++ ClassificationExperiments-Elena/code/Classification/scripts/DBAcc_FEBO_new.m
@@ -36,13 +36,26 @@ IDdate=[num2str(IDevice) '_' starttime(1:4) starttime(6:7) starttime(9:10)...
     '__' stoptime(1:4) stoptime(6:7) stoptime(9:10)...
     '_' stoptime(12:13) stoptime(15:16) stoptime(18:19)];
 
+
+fileName=[fullfile(results_path, 'S') IDdate '.kmz'];
+iconStr = ['http://maps.google.com/','mapfiles/kml/pal2/icon26.png']; 
+iconSize = 0.4;
+classText='stand flap soar walk sit XFl  float NoCl';
+dateTimeFormat = 'yyyy-mm-ddTHH:MM:SSZ';
+
 %mat-file for storing intermediate results
 StorName=[fullfile(results_path,'Stor') IDdate '.mat'];
 
 %name datacube
 DCubeName= [fullfile(results_path,'Datacube') IDdate];
 
-
+% name annotation file
+AnotName= [fullfile(results_path,'Anot') IDdate];
+% create png directory
+dirName=[results_path IDdate 'png'];
+if ~exist(dirName,'dir')
+    mkdir(dirName);
+end
 %% get the data from the DB
 
 t=clock;
@@ -84,6 +97,22 @@ tic
 Datacube = createDatacube(class_data,1,DCubeName);
 disp('Done.');
 toc
+
+%% create Anotations
+disp('Creating Annotations...');
+tic
+FTVanot = createAnot(FTVstor,num_features,1,AnotName);
+disp('Done.');                                
+toc
+
+%% Make kmz
+disp('Creating KMZ file...');
+tic
+makeKMZanot(class_data, iconStr, iconSize, ...
+                          classText, dateTimeFormat, dirName, fileName, true);
+disp('Done.');                                
+toc
+
 
 disp('Total elapsed time: ');
 etime(clock, t)
