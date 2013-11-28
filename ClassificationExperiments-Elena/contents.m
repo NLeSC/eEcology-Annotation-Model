fcf5ef7668cffd2f84aa927dcb3812790cecbb6e
elboyran <e.ranguelova@esciencecenter.nl>
2013-11-28 17:27:42 +0100

    added contents files

---
+++ ClassificationExperiments-Elena/contents.m
@@ -0,0 +1,19 @@
+%% contents.m- contents of the current directory
+%
+%--------------------------------------------------------------------------
+%% TEMPLATES
+%--------------------------------------------------------------------------
+% template.m-function/script header template
+%--------------------------------------------------------------------------
+
+%--------------------------------------------------------------------------
+%% FUNCTIONS
+%--------------------------------------------------------------------------
+% listSubFolders - returns list of all subfolders of a given folder
+%--------------------------------------------------------------------------
+%% SCRIPTS
+%--------------------------------------------------------------------------
+% startCheckcode.m - Matlab script for testing Matlab's code quality
+% startCheckcode.sh- shell script invoking the Matlab script with the same name
+% startTesting.m - Matlab script for testing Matlab's code functionality
+% startTetsing.sh- shell script invoking the Matlab script with the same name
